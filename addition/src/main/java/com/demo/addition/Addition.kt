83c1417ac3dbc156f830922188ccc8ca219d7f48
Sivakumar <loyltwo3ks.developers@gmail.com>
2026-01-28 14:28:58 +0530

    second commit

--- addition/src/main/java/com/demo/addition/Addition.kt
+++ addition/src/main/java/com/demo/addition/Addition.kt
@@ -5,4 +5,15 @@ object Addition {
     fun add(a:Int,b:Int):Int{
         return a + b
     }
+
+    fun subraction(a:Int,b:Int):Int{
+        return a - b
+    }
+
+    fun multiply(a:Int,b:Int):Int{
+        return a * b
+    }
+    fun divide(a:Int,b:Int):Int{
+        return a / b
+    }
 }
